3f76b9b1c016a9aa5c9c5907ddda036b0eef4f7b
RAKOTONDRAZAKA Nameno Fanantenana <mrnrakotondrazaka@gmail.com>
2025-02-19 23:05:26 +0300

    ajout de footer

--- src/layouts/ln.tsx
+++ src/layouts/ln.tsx
@@ -1,15 +1,25 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Navbar from "./navbar";
+import { useLocation } from "react-router-dom";
+import Footer from "./footer";
 
 interface LnProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
-function Ln({children}: LnProps) {
-    return ( <>
-        <Navbar />
-        {children}
-    </> );
+function Ln({ children }: LnProps) {
+  const location = useLocation();
+  const isNotRecette = location.pathname !== "/recette";
+
+  useEffect(() => {}, [location.pathname, isNotRecette]);
+
+  return (
+    <>
+      <Navbar />
+      {children}
+      {isNotRecette && <Footer />}
+    </>
+  );
 }
 
 export default Ln;
