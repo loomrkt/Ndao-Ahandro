39098618b70d910823bf2554f7b6f5b5387f97b1
RAKOTONDRAZAKA Nameno Fanantenana <mrnrakotondrazaka@gmail.com>
2025-02-20 12:16:29 +0300

    changement de faq en bento

--- src/Components/faqElement.tsx
+++ src/Components/faqElement.tsx
@@ -1,18 +1,39 @@
 import { JSX } from "react";
 
-function FaqElement():JSX.Element {
-    return ( <>
-        <div>
-            <h3 className="text-lg font-semibold ">
-            Can I cancel at anytime?
-            </h3>
-            <p className="mt-2 text-wp">
-            Yes, you can cancel anytime no questions are asked while you
-            cancel but we would highly appreciate if you will give us some
-            feedback.
+interface FaqElementProps {
+  id: string;
+}
+
+function FaqElement({ id }: FaqElementProps): JSX.Element {
+  return (
+    <>
+      <div className="accordion-item" id={`${id}`}>
+        <button
+          className="accordion-toggle inline-flex items-center justify-between text-start"
+          aria-controls={`${id}-collapse`}
+          aria-expanded="false"
+        >
+          How would you ship my order?
+          <span className="icon-[tabler--chevron-left] accordion-item-active:-rotate-90 size-5 shrink-0 transition-transform duration-300 rtl:-rotate-180"></span>
+        </button>
+        <div
+          id={`${id}-collapse`}
+          className="accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+          aria-labelledby={`${id}`}
+          role="region"
+        >
+          <div className="px-5 pb-4">
+            <p className="text-base-content/80 font-normal">
+              For large products, we deliver your product via a third party
+              logistics company offering you the “room of choice” scheduled
+              delivery service. For small products, we offer free parcel
+              delivery.
             </p>
+          </div>
         </div>
-    </> );
+      </div>
+    </>
+  );
 }
 
 export default FaqElement;
--- src/pages/home/section/faq.tsx
+++ src/pages/home/section/faq.tsx
@@ -2,24 +2,42 @@ import { JSX } from "react";
 import FaqElement from "../../../Components/faqElement";
 
 function Faq():JSX.Element {
-    return ( <>
-    <div className="max-w-[1000px] px-4 py-20 mx-auto">
-        <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
-        <h2 className="mb-4 text-3xl tracking-tight font-bold sm:text-4xl">
-        Les questions les plus fréquentes</h2>
-        </div>
-        <div className="max-w-5xl mx-auto">
-          <div className="grid sm:grid-cols-2 gap-6 md:gap-12">
-            <FaqElement/>
-            <FaqElement/>
-            <FaqElement/>
-            <FaqElement/>
-            <FaqElement/>
-            <FaqElement/>
+    return (
+      <>
+        <div className="max-w-[1200px] mx-4  px-4 py-20 md:mx-auto bg-base-100 rounded-lg shadow-sm">
+          <div className="flex items-start md:items-center md:justify-between  p-5 max-w-5xl mx-auto flex-col md:flex-row">
+            <div className="max-w-2xl text-center ">
+              <h6 className="text-start">FAQ</h6>
+              <h2 className="mb-4 text-3xl tracking-tight font-bold sm:text-4xl text-start">
+                Questions & <br />
+                <span className="opacity-55">Reponses</span>
+              </h2>
+            </div>
+            <div className="max-w-sm text-start ">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Molestias modi maxime animi ipsum delectus nobis quod autem
+                blanditiis architecto laboriosam! Ipsam iusto ullam voluptatibus
+                quisquam rem voluptatum placeat molestiae culpa!
+              </p>
+            </div>
+          </div>
+          <div className="max-w-5xl mx-auto">
+            {/* accordion */}
+            <div className="accordion divide-neutral/20 divide-y">
+              {[
+                "When is payment taken for my order?",
+                "How do I change my order?",
+                "How do I return an item?",
+              ].map((item, index) => (
+                <FaqElement id={`faq-${index}`} key={index} />
+              ))}
+            </div>
+            {/* accordion */}
           </div>
         </div>
-      </div>
-    </> );
+      </>
+    );
 }
 
 export default Faq;
